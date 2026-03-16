9aa04c1c290930e4a75beadec273d72b7a3f8770
LaureK-cdp <kauffmann.laure@gmail.com>
2026-03-16 18:57:12 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,10 @@
 import express from 'express';
-import helmet from 'helmet';
-import rateLimit from 'express-rate-limit';
 
 const app = express();
 app.use(express.json());
-app.use(helmet()); // Sécurité Question 6
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100
-});
-app.use(limiter);
-
-// Route d'exemple pour ton Kanban
-app.post('/api/taches', (req, res) => {
-  const { intitule } = req.body;
-  res.status(201).json({ message: "Tâche créée", data: { intitule, colonne: "À faire" } });
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "Système Kanban opérationnel" });
 });
 
-export default app;x
+export default app;
