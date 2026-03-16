821ae261ca2330051e9c70aae82485c1d4fb4518
LaureK-cdp <kauffmann.laure@gmail.com>
2026-03-16 19:55:10 +0100

    Create app.test.ts

---
+++ tests/app.test.ts
@@ -0,0 +1,6 @@
+import { describe, it, expect } from 'vitest';
+describe('Vérification technique', () => {
+  it('doit confirmer que les tests fonctionnent', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
